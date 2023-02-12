e5afbd822a7cbda26038f57d51b87a7298da5259
Damian Giaccobasso <dgiaccobasso@192.168.1.3>
2023-02-12 18:52:43 -0300

    add card animations

--- src/components/PuntaOffers.vue
+++ src/components/PuntaOffers.vue
@@ -4,13 +4,15 @@
       <hr />
       <div class="row">
         <div class="col-md-4" v-for="offer in offers" :key="offer.title">
-          <div class="card mb-4">
-            <img :src="offer.image" class="card-img-top" alt="offer image">
-            <div class="card-body">
-              <h5 class="card-title">{{ offer.title }}</h5>
-              <p class="card-text">{{ offer.description }}</p>
-              <div class="text-center">
-                <h6 class="font-weight-bold">{{ offer.discount }}% off</h6>
+          <div class="card-container">
+            <div class="card mb-4">
+              <img :src="offer.image" class="card-img-top" alt="offer image">
+              <div class="card-body">
+                <h5 class="card-title">{{ offer.title }}</h5>
+                <p class="card-text">{{ offer.description }}</p>
+                <div class="text-center">
+                  <h6 class="font-weight-bold">{{ offer.discount }}% off</h6>
+                </div>
               </div>
             </div>
           </div>
@@ -51,7 +53,23 @@
   </script>
   
   <style>
+  .card-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
   .card {
+    width: 300px;
+    height: 200px;
+    margin: 20px;
+    border-radius: 10px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    transition: transform 0.3s;
+  }
+  .card:hover {
+    transform: scale(1.1);
   }
   </style>
