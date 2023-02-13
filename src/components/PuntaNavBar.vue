916d1fefcc0d4ad6e8e1fb68913455914c5c0c23
Damian Giaccobasso <dgiaccobasso@192.168.1.3>
2023-02-13 12:09:26 -0300

    fix navbar

--- src/components/PuntaNavBar.vue
+++ src/components/PuntaNavBar.vue
@@ -1,14 +1,13 @@
 <template> 
-    <nav>
-      <div class="logo">
-        <img src="../../public/puntaicon.png" alt="Logo">
-      </div>
-        
-    <div class="buttons">
-      <a href="#">Sign In</a>
-      <a href="#">Log In</a>
-    </div>
-  </nav>
+    <nav class="d-flex justify-content-between align-items-center" style="padding: 1rem;">
+  <div class="logo">
+    <img src="../../public/puntaicon.png" alt="Logo">
+  </div>
+  <div>
+    <button class="btn btn-primary me-2">Log In</button>
+    <button class="btn btn-primary">Sign Up</button>
+  </div>
+</nav>
   </template>
 
 <script>
@@ -17,29 +16,12 @@ export default {};
 
   
   <style>
-    nav {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem;
-  }
+    
   
   .logo {
     font-size: 1.5rem;
     font-weight: bold;
   }
   
-  .buttons {
-    display: flex;
-  }
-  
-  a {
-    padding: 0.5rem 1rem;
-    background-color: #3498db;
-    color: white;
-    border-radius: 5px;
-    text-decoration: none;
-    margin-left: 1rem;
-  }
 
   </style>  
