eabe5627fc63a263e272da81bfdaa71286b5e7cf
Damian Giaccobasso <dgiaccobasso@192.168.1.3>
2023-02-13 10:14:18 -0300

    add config to main.js for github pages

--- src/main.js
+++ src/main.js
@@ -2,3 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+if (process.env.NODE_ENV === 'production') {
+  // eslint-disable-next-line no-undef
+  __webpack_public_path__ = `https://dgiacco.github.io/Punta-Pass/`;
+}
